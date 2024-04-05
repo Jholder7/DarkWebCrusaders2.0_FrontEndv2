62f21709824afe517b8f068a7cac59c5a9f9b225
jholder7 <jholder7@murraystate.edu>
2024-04-04 21:22:16 -0500

    Finished Loading Screen

--- src/routes/application/Application.js
+++ src/routes/application/Application.js
@@ -84,11 +84,11 @@ class Application extends React.Component {
         this.timer = null;
         this.editor = null;
     }
-
+    Programtastic
     componentDidMount = () => {
         // We can just pull all needed data and update out finish flash using the async finish function, the update this variable when we have all the data
         // We sorta want the whole screen to load so that way we can inject the data as we receive it so its ready once done loading.
-        setTimeout(() => {this.setState({isLoading: false})}, 1000);
+        setTimeout(() => {this.setState({isLoading: false})}, 10000);
         request(
             "GET",
             "/api/v1/application/baseUserData",
@@ -152,7 +152,7 @@ class Application extends React.Component {
                     {/*This div covers the whole screen while isLoading any child components under here will be hidden when loading is done*/}
                     {/*Add any loading animations, svgs, or gifs here and styler her up*/}
                     <div className="spinner">
-                        <span>The Power of Programtastic. . .</span>
+                        <span>Programtastic</span>
                         <div className="half-spinner"></div>
                     </div>
                 </div>
